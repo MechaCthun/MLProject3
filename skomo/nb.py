e3e1ead446544ff5479c1f0e4e8c391ff62211d3
nSkomo <89427563+nSkomo@users.noreply.github.com>
2024-12-01 18:16:39 -0500

    Add files via upload

---
+++ skomo/nb.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report, accuracy_score
+
+file_path = 'l1_processed_urls.csv'
+data = pd.read_csv(file_path)
+
+z = data[["type_benign","type_defacement","type_malware","type_phishing","type_type"]]
+data.drop(columns=["type_benign","type_defacement","type_malware","type_phishing","type_type"], inplace=True)
+X = data
+
+y = z.idxmax(axis=1)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+# Print results
+print("Classification Report:\n", classification_report(y_test, y_pred))
+print("Accuracy Score:", accuracy_score(y_test, y_pred))
